55bf9b9eb8259971f4f107da600904674326ccd5
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2024-12-01 16:03:09 -0300

    refactor: renomeia nome da aplicacao

--- src/main/java/com/avalanches_pagamento/frameworksanddrivers/api/AvalanchesPagamentoApplication.java
+++ src/main/java/com/avalanches_pagamento/frameworksanddrivers/api/AvalanchesPagamentoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.avalanches_pagamento")
-public class AvalanchesApplication {
+public class AvalanchesPagamentoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvalanchesApplication.class, args);
+		SpringApplication.run(AvalanchesPagamentoApplication.class, args);
 	}
 
 }
